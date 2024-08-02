294972ee1baf5e4648e312ed759f58b48d06481e
darren <darren.kooyz@gmail.com>
2024-08-02 16:16:15 +0800

    fix: css

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,8 @@
 import './bootstrap';
 import '../css/app.css';
 import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
