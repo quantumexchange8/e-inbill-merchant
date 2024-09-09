13280717c2f4c5ab568c6692cc2917fec509b8ee
darren <darren.kooyz@gmail.com>
2024-09-09 17:55:01 +0800

    fix: remove dd

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -124,7 +124,6 @@ class ItemController extends Controller
 
     public function editItem(Request $request)
     {
-        dd($request->all());
         $item = Item::find($request->id);
         $item->update([
             'merchant_id' => Auth::user()->merchant_id,
