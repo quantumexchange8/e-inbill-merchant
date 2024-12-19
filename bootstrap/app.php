1228a155eef2f21e5a66781852bf625c8426e51d
darren <darren.kooyz@gmail.com>
2024-12-19 13:40:17 +0800

    fix: close clearlog

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             // \App\Http\Middleware\CheckPermission::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-            \App\Console\Commands\ClearLogs::class,
+            // \App\Console\Commands\ClearLogs::class,
         ]);
 
         //
